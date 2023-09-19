2327733e2b5d4ecaed1c2d6185c906dede500763
romain <romain.quentel.bzh29@gmail.com>
2023-09-19 22:28:43 +0200

    Update experiences

--- pages/cv/oxxeo.vue
+++ pages/cv/oxxeo.vue
@@ -3,11 +3,16 @@
     <template v-slot:title>Oxxeo</template>
     <template v-slot:dates>Février 2019 - Aujourd'hui</template>
     <template v-slot:content>
-      Depuis 2019, je fais partis de l'aventure Oxxeo. Cela m'a permis d'effectuer des missions variées atours de PHP et
-      de Symfony, tout en m'améliorant sur plein d'autres technologies de développement front-end, de déploiement continue, ect...
-      Découvrez en détail les missions que j'ai pu accomplir chez <NuxtLink to="/cv/oxxeo-jouve">Luminess (ex Jouve)</NuxtLink>,
-      au <NuxtLink to="/cv/oxxeo-rectorat-rennes">Rectorat de Rennes</NuxtLink>, chez <NuxtLink to="/cv/oxxeo-additi">Additi</NuxtLink> et
-      maintenant chez <NuxtLink to="/cv/oxxeo-nehs-digital">Nehs Digital</NuxtLink>.
+      <p>
+        Depuis 2019, je fais partis de l'aventure Oxxeo. Cela m'a permis d'effectuer des missions variées atours de PHP et
+        de Symfony, tout en m'améliorant sur plein d'autres technologies de développement front-end, de déploiement continue, ect...
+        Découvrez en détail les missions que j'ai pu accomplir chez <NuxtLink to="/cv/oxxeo-jouve">Luminess (ex Jouve)</NuxtLink>,
+        <NuxtLink to="/cv/oxxeo-scalian">Scalian</NuxtLink>, <NuxtLink to="/cv/oxxeo-additi">Additi</NuxtLink> ou encore
+        <NuxtLink to="/cv/oxxeo-nehs-digital">Nehs Digital</NuxtLink>.
+      </p>
+      <p>
+        L'ensemble des missions que j'ai eu l'opportunité d'effectuer sont disponibles dans le menu ci-contre.
+      </p>
     </template>
   </experience-detail>
 </template>
